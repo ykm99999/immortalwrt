5bbe5b0ed02c2b5ee7bd192947008a38b5f037c8
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-02-06 19:19:04 +0800

    Update filogic.mk

--- filogic.mk
+++ filogic.mk
@@ -3,20 +3,28 @@ define Device/sl3000-emmc
   DEVICE_MODEL := Custom-1GB-Edition
   DEVICE_DTS := mt7981b-sl3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,sl3000-emmc mediatek,mt7981b
+  SUPPORTED_DEVICES := sl,sl3000-emmc mediatek,mt7981b mediatek,mt7981
   
-  # 【数字化关键】128M -> 131072k | 1024M -> 1048576k
-  KERNEL_SIZE := 131072k
-  IMAGE_SIZE := 1048576k
+  # 锁定 128MB 内核分区对齐成果
+  KERNEL_SIZE := 134217728
+  IMAGE_SIZE := 1073741824
   
+  # 强制内核 LZMA 压缩
+  KERNEL := kernel-bin | lzma | fit $$(DEVICE_DTS)
+  
+  # 核心驱动包集成
   DEVICE_PACKAGES := \
 	kmod-mmc kmod-sdhci-mtk \
+	kmod-mt753x \
 	kmod-fs-f2fs f2fs-tools f2fsck \
 	kmod-usb3 kmod-usb-dwc3-mtk \
 	block-mount blkid lsblk parted
   
-  IMAGES := factory.bin sysupgrade.bin
-  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | pad-rootfs | check-size | mtk-sdcard
-  IMAGE/sysupgrade.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | pad-rootfs | check-size | mtk-sdcard
+  # 生成两种格式固件
+  IMAGES := sysupgrade.bin sysupgrade.bin.gz
+  
+  # 拼装逻辑：确保内核 pad 到 128M 偏移处
+  IMAGE/sysupgrade.bin := append-kernel | pad-to 134217728 | append-rootfs | append-metadata
+  IMAGE/sysupgrade.bin.gz := append-kernel | pad-to 134217728 | append-rootfs | append-metadata | gzip
 endef
 TARGET_DEVICES += sl3000-emmc
