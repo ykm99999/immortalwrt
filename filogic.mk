520ec5492c94bc3dee80f864a5289ad9df7aa513
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-02-13 01:34:11 +0800

    Update filogic.mk

--- filogic.mk
+++ filogic.mk
@@ -1,19 +1,19 @@
 define Device/sl3000-emmc
-  DEVICE_VENDOR := 司络
-  DEVICE_MODEL := SL3000 eMMC
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000 (eMMC)
+  DEVICE_ALT0_VENDOR := SL
+  DEVICE_ALT0_MODEL := SL3000
+  DEVICE_ALT0_VARIANT := eMMC 1024MB Rootfs
   DEVICE_DTS := mt7981b-sl3000-emmc
-  SUPPORTED_DEVICES := sl,sl3000-emmc mediatek,mt7981b sl3000-emmc
-
-  KERNEL_SIZE := 134217728
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := sl,3000-emmc mediatek,mt7981
+  KERNEL_SIZE := 131072k
   BOARD_ROOTFS_PARTSIZE := 1024
-
-  # 🎯 物理修复：单 $ 符号
-  KERNEL := kernel-bin | lzma | fit $(DEVICE_DTS)
-  KERNEL_INITRAMFS :=
-
-  DEVICE_PACKAGES := kmod-mmc kmod-sdhci-mtk kmod-fs-f2fs f2fs-tools parted lsblk blkid block-mount kmod-zram zram-swap
-
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := append-kernel | pad-to 134217728 | append-rootfs | append-metadata
+  KERNEL := kernel-bin | lzma | append-dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | append-dtb
+  DEVICE_PACKAGES := kmod-mmc kmod-sdhci-mtk kmod-fs-f2fs f2fs-tools f2fsck parted lsblk blkid block-mount kmod-zram zram-swap
+  IMAGES := sysupgrade.bin factory.bin
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | append-metadata
+  IMAGE/factory.bin := append-kernel | append-rootfs
 endef
 TARGET_DEVICES += sl3000-emmc
